dc3bcba030b9ed51eb9da81cee9f718daf6611d2
Kassie Bradshaw <kassie.r.bradshaw@gmail.com>
2022-06-30 15:29:51 -0700

    Uploaded class 8 demo

--- app.js
+++ app.js
@@ -1,62 +1,15 @@
-let usersColor = prompt("What is your favorite color?");
+// STRUCTURE OF A FUNCTION
+// function askTime(){}
+// -- keyword |name|parameters|codeblock to be executed
+// -- name is whatever you want it to be
+// -- parameters are the (optional) additional data you'll feed into the function
+// -- codeblock is what you want your function to do
 
-// If the user's fav color is yellow, say "Mine too!"
-// otherwise say "That's a good color"
+//---------------------------------------------------------------------------------
 
-// This block of code is using conditional logic to write a message to the console
-
-// if (usersColor == "YELLOW"){
-//     console.log("I also like YELLOW!!!!");
-// } else if (usersColor.toLowerCase() == "yellow") {
-//     console.log("Mine too!");
-// } else {
-//     console.log("That's still a nice color.");
-// }
-
-// This block of code is using conditional logic to write a message to the web page
-if (usersColor == "YELLOW"){
-    document.write("I also like YELLOW!!!!");
-} else if (usersColor.toLowerCase() == "yellow") {
-    document.write("Mine too!");
-} else {
-    document.write("That's still a nice color.");
-}
+// -------- DECLARING THE FUNCTION --------
 
 // Ask the user what time it is, and give them a message with conditional logic
-
-// Structure of a function
-//keyword /name /parameters /codeblock
-// function askTime(){}
-
-
-//   parenthesis(parameters)
-function addNumbers(x, y){
-    return x + y;
-}
-
-// parenthesis(arguments)
-// addNumbers(4, 8);
-
-// let classGreeting = addNumbers("Hello ", "Class");
-// console.log("LINE 41", classGreeting);
-
-// document.write(addNumbers("Hello ", "Class"));
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// DECLARING THE FUNCTION
 function askTime(){
     // code the function executes 
     let time = prompt('What time is it?');
@@ -70,19 +23,78 @@ function askTime(){
         message = 'Good Evening!';
     } else {
         message = 'Time does not exist!';
+        askTime();
     }
-    document.write(message);
-    return message;
+    document.write(message); // write the message to the HTML document
+    return message;          // return the message from the function
 };
 
-// USING YOUR FUNCTION/ "invoke" / "call"
-// console.log(askTime());
+// ------------------------ INVOKING THE FUNCTION (referred to as calling it, means to use it) -------------------------------- 
 
-// Try to use a variable from inside the function, doesn't work
-// console.log(message);
+askTime();
 
-// Invoke the function, assign the result to a variable
-// let message = askTime();
-// Write message to console.
-// console.log("LINE 56", message);
+// ------------------------------------------------ TALKING ABOUT PARAMETERS ------------------------------------------
+
+//  parameters go in parenthesis --> tell the function it will need extra data, and give it a "placeholder" name
+function addNumbers(x, y){    // I'm going to need 2 numbers, I don't know what they are yet so I'll call them x & z 
+    return x + y;             // add the 2 numbers together and return the result 
+}
+
+// ---------------------------------------- INVOKE FUNCTION WITH PARAMETERS --------------------------------
+
+let sum = addNumbers(10, 35); // invoke the function with the 2 numbers I want to add together. Assign that result to a variable.
+console.log(sum);             // show what the sum of the function was
+
+// ------------------------------ ASK THE USER THEIR FAV COLOR ------------------------------------
+
+    // function to ask the user their fav color
+    // If the user's fav color (turned lowercase) is yellow, say "Mine too!"
+    // otherwise say "That's still a nice color"
+
+function favColor(){
+    let usersColor = prompt("What is your favorite color?");
+    
+    // This block of code is using the conditional logic to write a message to the console
+    // if (usersColor.toLowerCase() == "yellow") {
+    //     console.log("Mine too!");
+    // } else {
+    //     console.log("That's still a nice color.");
+    // }
+    
+    // This block of code is using the conditional logic to write a message to the web page
+    if (usersColor.toLowerCase() == "yellow") {
+        document.write("Mine too!");
+    } else {
+        document.write("That's still a nice color.");
+    }
+}
+
+// ------------------------------------ SHOW A FUNNY PIC -----------------------------------------------
+
+// function to show a pic to user with a for-loop
+// prints the picture to the HTML document
+
+function showPic(){
+    const userNum = prompt("How many times do you wanna see a funny picture?");
+    for(let i = 0; i < userNum; i++){
+        document.write("<img src='embarrassing.jpeg' style='width:300px;'/>")
+    }
+}
+
+// ----------- EXAMPLE OF HOW YOU CAN CONTROL WHEN YOUR FUNCTIONS RUN & WHERE (SCRIPT) ---------------------
+//
+// alert("Hi everyone!")
+// askTime();
+// favColor();
+// addNumbers(5, 9);
+// alert("Doin' some stuff...")
+// favColor();
+// addNumbers()
+// alert("See where I'm goin' with this...")
+
+
+
+
+
+
 
